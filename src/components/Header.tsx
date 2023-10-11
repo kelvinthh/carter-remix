06a7db57ab94ff30bb26d171d9f68310f5702240
Kelvin H H Tam <hoheitam@alumni.vtc.edu.hk>
2023-10-11 07:46:36 -0400

    Add scroll back to top button
    - Add scroll back to top button
    - Update H1 text size at medium screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 export default function Header() {
   return (
     // The real website left bar does not scale at small screen
-    // So I add a little breakpoint here just to give more space to content at sm
-    <header className="top-0 z-10 flex h-12 animate-fade flex-row items-center justify-between border-indigo-700 bg-indigo-950 px-4 text-white transition-all animate-ease-in-out sm:left-0 sm:h-full sm:min-h-screen sm:w-24 sm:flex-col sm:justify-center sm:border-r-2 sm:px-0 sm:py-6">
+    // So move the left bar to the top at small screen
+    <header className="top-0 z-10 flex h-12 animate-fade flex-row items-center justify-between border-indigo-700 bg-indigo-950 px-4 text-white transition-all duration-700 animate-ease-in-out sm:left-0 sm:h-full sm:min-h-screen sm:w-24 sm:flex-col sm:justify-center sm:border-r-2 sm:px-0 sm:py-6">
       {/* Icon */}
       <div className="h-10 w-10 animate-fade-right rounded-full bg-black animate-delay-200 sm:mb-auto" />
+
       {/* Main button group */}
       <div className="flex animate-fade-right flex-row space-x-4 animate-delay-500 animate-ease-in-out sm:mt-36 sm:flex-col sm:space-x-0 sm:space-y-8">
         <button>Button</button>
@@ -12,15 +13,20 @@ export default function Header() {
         <button>Button</button>
         <button>Button</button>
       </div>
+
       {/* Spacer */}
       <div className="hidden flex-grow bg-white sm:flex" />
+
       {/* Bottom buttons */}
       <div className="flex animate-fade-right flex-row items-center space-x-4 animate-delay-700 animate-ease-in-out sm:flex-col sm:space-x-0 sm:space-y-4">
+        {/* Get Pro Button */}
         <button className="flex animate-pulse items-center justify-center rounded-lg bg-white p-2 sm:animate-wiggle-more sm:animate-infinite">
           <span className="bg-gradient-to-r from-blue-500 from-30% via-green-500 to-cyan-500 bg-clip-text text-xs font-bold text-transparent sm:text-base">
             Get Pro
           </span>
         </button>
+
+        {/* Icon */}
         <div className="h-8 w-8 rounded-full bg-white sm:mb-auto" />
       </div>
     </header>
--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,16 +1,63 @@
+import { useState, useEffect, useRef } from "react";
+import { AiOutlineToTop } from "react-icons/ai";
+
 export default function Content() {
+  const [scrollButton, setScrollButton] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // Keep track of the scroll pos
+  useEffect(() => {
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", toggleVisibility);
+    }
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", toggleVisibility);
+      }
+    };
+  }, [containerRef]);
+
+  // Function to handle the scroll event and set the visibility state
+  // Window object does not work because this is a overflow container
+  
+  const toggleVisibility = () => {
+    const container = containerRef.current;
+    if (container && container.scrollTop > 400) {
+      setScrollButton(true);
+    } else {
+      setScrollButton(false);
+    }
+  };
+
   return (
-    <div className="flex h-screen w-full animate-fade flex-col items-center overflow-auto bg-black pb-16 text-white animate-delay-500">
+    <div
+      className="flex h-screen w-full animate-fade flex-col items-center overflow-y-auto bg-black pb-16 text-white animate-delay-500"
+      ref={containerRef}
+    >
+      {/* Scroll to top button */}
+      <button
+        className={`fixed bottom-8 right-8 z-50 rounded-full bg-violet-700 p-4 opacity-70 drop-shadow transition-all duration-500 hover:scale-110 hover:opacity-100 sm:bottom-16 ${
+          !scrollButton && `hidden cursor-default`
+        }`}
+        onClick={() => {
+          if (containerRef.current) {
+            containerRef.current.scrollTo({ top: 0, behavior: "smooth" });
+          }
+        }}
+      >
+        <AiOutlineToTop size="1.75em" color="white" />
+      </button>
+
       {/* Earn $1000 Text */}
       <button className="group w-full bg-gradient-to-r from-purple-600 via-pink-600 to-red-600 py-1 text-center font-bold">
         <p className="animate-pulse transition-all group-hover:animate-none">
           Share Your Characters & Earn up to $1,000! 🎉
         </p>
       </button>
-
       {/* A heading text block */}
       <div className="flex w-full flex-col items-center justify-center space-y-2 py-16 text-center font-light sm:space-y-4">
-        <h1 className="animate-fade-down text-4xl font-bold animate-delay-[500ms] animate-ease-in-out sm:text-6xl">
+        <h1 className="animate-fade-down text-4xl font-bold animate-delay-[500ms] animate-ease-in-out sm:text-5xl md:text-6xl">
           Chat With{" "}
           <span className="animate-pulse bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text text-transparent">
             Imaginary
@@ -27,7 +74,6 @@ export default function Content() {
           crowbar?
         </h3>
       </div>
-
       <h1>Test</h1>
       <h1>Test</h1>
       <h1>Test</h1>
